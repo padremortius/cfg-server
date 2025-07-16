c1c88d0755fec0c4a42fe1e89a6465eddb8611ac
Artem Misko <nospam@misko.su>
2025-07-17 01:13:33 +0300

    Add new tests

--- internal/common/common_test.go
+++ internal/common/common_test.go
@@ -30,3 +30,55 @@ func TestStructToJSONBytes(t *testing.T) {
 		})
 	}
 }
+
+func TestStructToYamlBytes(t *testing.T) {
+	type args struct {
+		v interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{name: "Test 1", args: args{v: 1}, want: []byte("1\n"), wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StructToYamlBytes(tt.args.v)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StructToYamlBytes() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("StructToYamlBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStructToTomlBytes(t *testing.T) {
+	type args struct {
+		v interface{}
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		{name: "Test 1", args: args{v: 1}, want: []byte("1"), wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := StructToTomlBytes(tt.args.v)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("StructToTomlBytes() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("StructToTomlBytes() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
